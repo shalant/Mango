9c65ddfeba8216a771563330483eb7a308c7d8ad
youtube <youtube@example.com>
2023-07-24 18:26:10 -0500

    fixing contributions

--- Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -4,6 +4,6 @@
     {
         public Object? Result { get; set; }
         public bool IsSuccess { get; set; } = true;
-        public string Message { get; set; } = "blah";
+        public string Message { get; set; }
     }
 }
