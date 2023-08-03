fb5c61a4b1c60f57ad0b3ff17bacc234a6c6e7b9
youtube <youtube@example.com>
2023-08-03 16:09:15 -0500

    starting identity

---
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace Mango.Services.AuthAPI.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            
+        }
+
+    }
+}
