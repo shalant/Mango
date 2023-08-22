8c61da6c361eb47364942f0cc1b5be7b8e48409a
youtube <youtube@example.com>
2023-08-22 09:20:55 -0500

    Finished module 125

---
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+    }
+}
---
+++ Mango.Services.OrderAPI/Migrations/20230822135722_addOrderTables3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.OrderAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOrderTables3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductPrice",
+                table: "OrderDetails",
+                newName: "Price");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "OrderDetails",
+                newName: "ProductPrice");
+        }
+    }
+}
